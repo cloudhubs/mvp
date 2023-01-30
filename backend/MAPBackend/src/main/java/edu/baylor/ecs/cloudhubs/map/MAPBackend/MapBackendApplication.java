4b23e75f26bd859fde7e417f129f279daa6e04eb
patrickeharris <patrickeharris404@gmail.com>
2023-01-30 14:19:36 -0600

    Add backend

---
+++ backend/MAPBackend/src/main/java/edu/baylor/ecs/cloudhubs/map/MAPBackend/MapBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.baylor.ecs.cloudhubs.map.MAPBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapBackendApplication.class, args);
+	}
+
+}
---
+++ backend/MAPBackend/src/main/java/edu/baylor/ecs/cloudhubs/map/MAPBackend/ServletInitializer.java
@@ -0,0 +1,13 @@
+package edu.baylor.ecs.cloudhubs.map.MAPBackend;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MapBackendApplication.class);
+	}
+
+}
