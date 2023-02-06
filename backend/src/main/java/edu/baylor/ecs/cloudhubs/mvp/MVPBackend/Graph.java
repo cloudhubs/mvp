d20d5076a9358b1ece74849b9c836bbf912cd6e9
Gabriel Goulis <ninjagabe2003@gmail.com>
2023-02-05 18:45:35 -0600

    Node class, modified graph class to store nodeGraph

--- backend/src/main/java/edu/baylor/ecs/cloudhubs/mvp/MVPBackend/Graph.java
+++ backend/src/main/java/edu/baylor/ecs/cloudhubs/mvp/MVPBackend/Graph.java
@@ -8,8 +8,20 @@ import java.io.FileReader;
 import java.util.*;
 
 public class Graph {
-    Map<String, Set<String>> depend = new HashMap<>();
-    Map<String, Set<String>> target = new HashMap<>();
+    private Map<String, Set<String>> depend = new HashMap<>();
+    private Map<String, Set<String>> target = new HashMap<>();
+
+    private Map<Node, Node> nodeGraph = new HashMap<>();
+
+    private List<Node> nodes = new ArrayList<>();
+
+
+
+    /*
+        Graph constructor
+        Assumptions: Throws exception on invalid graph
+        given that it is loaded from a static file
+     */
     public Graph() throws Exception {
         Object obj = new JSONParser().parse(new
                 FileReader("src/main/java/edu/baylor/ecs/cloudhubs/mvp" +
@@ -18,10 +30,14 @@ public class Graph {
 
         JSONArray nodes = ((JSONArray)data.get("nodes"));
 
+        // Iterate through all the JSON nodes
         for (Object node : nodes) {
             JSONObject n = (JSONObject) node;
+            Node newNode = jsonToNode(n);
             JSONArray deps = ((JSONArray)n.get("dependencies"));
             JSONArray tars = ((JSONArray)n.get("targets"));
+            nodes.add(newNode);
+
             if(deps != null) {
                 depend.put((String) n.get("nodeName"), arr2Set(deps));
             }
@@ -29,6 +45,8 @@ public class Graph {
                 target.put((String) n.get("nodeName"), arr2Set(tars));
             }
         }
+
+
     }
 
     public Set<String> arr2Set(JSONArray arr){
@@ -38,5 +56,35 @@ public class Graph {
             ret.add((String)jo.get("nodeName"));
         }
         return ret;
+    }
+
+    public Node jsonToNode(JSONObject obj){
+        Set<Node> dependenciesSet = new HashSet<>();
+        Set<Node> targetsSet = new HashSet<>();
+        JSONArray dependencies = (JSONArray) obj.get("dependencies");
+        JSONArray targets = (JSONArray) obj.get("targets");
+
+        for (Object o: dependencies) {
+
+        }
+
+        return new Node((String) obj.get("nodeName"), (String) obj.get("nodeType"),
+                (Set<Node>) obj.get("dependencies"), (Set<Node>) obj.get("target"));
+    }
+
+    public Map<String, Set<String>> getDepend() {
+        return depend;
+    }
+
+    public void setDepend(Map<String, Set<String>> depend) {
+        this.depend = depend;
+    }
+
+    public Map<String, Set<String>> getTarget() {
+        return target;
+    }
+
+    public void setTarget(Map<String, Set<String>> target) {
+        this.target = target;
     }
 }
--- backend/src/main/java/edu/baylor/ecs/cloudhubs/mvp/MVPBackend/GraphAlgorithms.java
+++ backend/src/main/java/edu/baylor/ecs/cloudhubs/mvp/MVPBackend/GraphAlgorithms.java
@@ -1,5 +1,53 @@
 package edu.baylor.ecs.cloudhubs.mvp.MVPBackend;
 
 public class GraphAlgorithms {
-    public void SCC(){}
+    public static void SCC(Graph graph){
+
+
+        /*
+        if (vertex && !this.list[vertex]) {
+            console.log(vertex);
+            throw new Error("Vertex DNE");
+        }
+
+        vertex = vertex
+            ? vertex.toString()
+            : Object.keys(this.list)[0].toString();
+
+
+        // Create Copy of current Adjacency list
+
+        var reverseEdgeGraph = getReverseGraph(this.list);
+        var stack = [];
+        var visited = {};
+        for (var vertex in this.list) {
+            this.dfs(
+                    null,
+                    vertex,
+                    function (v) {
+                stack.push(v);
+            },
+            visited
+            );
+        }
+
+        var allSCC = [];
+        visited = {};
+        stack.reverse().forEach((vertex) => {
+                var SCC = [];
+        this.dfs(
+                reverseEdgeGraph,
+                vertex,
+                function (v) {
+            SCC.push(v);
+        },
+        visited
+            );
+        if (SCC.length) allSCC.push(SCC);
+        });
+
+        return allSCC;
+    }
+         */
+    }
 }
