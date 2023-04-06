dba9c31d7210348056962397f85a6091300bce59
Austin Huizinga <42579548+austinth127@users.noreply.github.com>
2023-04-06 17:46:00 -0500

    small fix

--- backend/src/main/java/edu/baylor/ecs/cloudhubs/mvp/MVPBackend/antipatterns/model/patterns/AntiPattern.java
+++ backend/src/main/java/edu/baylor/ecs/cloudhubs/mvp/MVPBackend/antipatterns/model/patterns/AntiPattern.java
@@ -2,7 +2,7 @@ package edu.baylor.ecs.cloudhubs.mvp.MVPBackend.antipatterns.model.patterns;
 
 public interface AntiPattern {
     /**
-     * Get the name of this antipattern
+     * Get the name of this anti-pattern
      * @return name
      */
     String getName();
