6e277b1f363a0a3ad8af5fddad7230ec5a4d1c54
Austin Huizinga <42579548+austinth127@users.noreply.github.com>
2023-04-10 16:50:50 -0500

    del

--- backend/src/main/java/edu/baylor/ecs/cloudhubs/mvp/MVPBackend/exceptions/ExceptionResponse.java
+++
@@ -1,9 +0,0 @@
-package edu.baylor.ecs.cloudhubs.mvp.MVPBackend.exceptions;
-
-import java.time.LocalDateTime;
-
-public record ExceptionResponse(LocalDateTime timestamp,
-                                String message,
-                                String details) {
-
-}
